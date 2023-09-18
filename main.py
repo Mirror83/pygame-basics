3ec9d814cb7576aebfd594fb0fcbab77ffba272b
Mirror83 <glenochieng045@gmail.com>
2023-09-18 06:44:42 +0300

    Add simple animation for the snail

--- main.py
+++ main.py
@@ -13,12 +13,17 @@ text_font = pygame.font.Font("assets/fonts/Pixeltype.ttf", 50)
 clock = pygame.time.Clock()
 MAX_FPS = 60
 
-sky_surface = pygame.image.load("assets/graphics/sky.png")
-ground_surface = pygame.image.load("assets/graphics/ground.png")
+# Convert images into a format that pygame can with easily
+sky_surface = pygame.image.load("assets/graphics/sky.png").convert_alpha()
+ground_surface = pygame.image.load("assets/graphics/ground.png").convert_alpha()
 
 # Antialiasing has been set to False only because this is a pixel art game
 # For other applications, antialiasing should be set to True
 text_surface = text_font.render("My game", False, "Black")
+
+snail_surface = pygame.image.load("assets/graphics/snail/snail1.png").convert_alpha()
+snail_x_pos = 600
+snail_speed = 2
 
 while True:
     # Event loop
@@ -31,6 +36,11 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, HEIGHT - 100))
     screen.blit(text_surface, (WIDTH / 2 - text_surface.get_rect().width / 2, 20))
+    if snail_x_pos < -100:
+        snail_x_pos = WIDTH
+
+    snail_x_pos -= snail_speed
+    screen.blit(snail_surface, (snail_x_pos, 260))
 
     # Update screen
     pygame.display.update()
