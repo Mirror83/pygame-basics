c8b293a849cb736157d270c272596da152c486d1
Mirror83 <glenochieng045@gmail.com>
2023-09-18 06:23:07 +0300

    Added text with a custom font type

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ HEIGHT = 400
 screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 pygame.display.set_caption("Runner")
 
-sky_surface = pygame.image.load("assets/graphics/sky.png")
-ground_surface = pygame.image.load("assets/graphics/ground.png")
+text_font = pygame.font.Font("assets/fonts/Pixeltype.ttf", 50)
 
 clock = pygame.time.Clock()
 MAX_FPS = 60
+
+sky_surface = pygame.image.load("assets/graphics/sky.png")
+ground_surface = pygame.image.load("assets/graphics/ground.png")
+
+# Antialiasing has been set to False only because this is a pixel art game
+# For other applications, antialiasing should be set to True
+text_surface = text_font.render("My game", False, "Black")
 
 while True:
     # Event loop
@@ -22,11 +28,12 @@ while True:
             exit()
 
     # Draw elements
-    # Place test_surface on the screen
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, HEIGHT - 100))
+    screen.blit(text_surface, (WIDTH / 2 - text_surface.get_rect().width / 2, 20))
+
     # Update screen
     pygame.display.update()
 
-    # Ensure that the while loop runs at the framerate of MAX_FPS
+    # Ensure that the while loop runs at a maximum framerate of MAX_FPS
     clock.tick(MAX_FPS)
