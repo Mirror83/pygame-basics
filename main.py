ca55eac1d59ee875b31818df58d847394d7616a6
Mirror83 <glenochieng045@gmail.com>
2023-09-20 21:31:34 +0300

    Added basic logic to handle the game over state

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.init()
 
 WIDTH = 800
 HEIGHT = 400
-GROUND_POSISTION = 300
+GROUND_POSITION = 300
 screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
 pygame.display.set_caption("Runner")
 
@@ -28,13 +28,15 @@ score_surface = text_font.render("My game", False, text_color)
 score_rectangle = score_surface.get_rect(center=(WIDTH / 2, 50))
 
 snail_surface = pygame.image.load("assets/graphics/snail/snail1.png").convert_alpha()
-snail_rectangle = snail_surface.get_rect(midbottom=(600, GROUND_POSISTION))
+SNAIL_START_POSITION = (WIDTH, GROUND_POSITION)
+snail_rectangle = snail_surface.get_rect(midbottom=SNAIL_START_POSITION)
 snail_speed = 4
 
 player_surface = pygame.image.load("assets/graphics/player/player_walk_1.png").convert_alpha()
-player_rectangle = player_surface.get_rect(midbottom=(80, GROUND_POSISTION))
+PLAYER_START_POSITION = (80, GROUND_POSITION)
+player_rectangle = player_surface.get_rect(midbottom=PLAYER_START_POSITION)
 player_gravity = 0
-has_collided = False
+game_active = True
 
 while True:
     # Event loop
@@ -43,44 +45,55 @@ while True:
             pygame.quit()
             exit()
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == pygame.BUTTON_LEFT:
-                if player_rectangle.bottom >= GROUND_POSISTION:   
-                    if player_rectangle.collidepoint(event.pos):
-                        print("Clicked on player")
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == pygame.BUTTON_LEFT:
+                    if player_rectangle.bottom >= GROUND_POSITION:   
+                        if player_rectangle.collidepoint(event.pos):
+                            print("Clicked on player")
+                            player_gravity = -20
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if player_rectangle.bottom >= GROUND_POSITION: 
                         player_gravity = -20
-
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                if player_rectangle.bottom >= GROUND_POSISTION: 
-                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    snail_rectangle.midbottom = SNAIL_START_POSITION
+                    player_rectangle.midbottom = PLAYER_START_POSITION
+                    game_active = True
             
-    # Draw elements
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, GROUND_POSISTION))
+    if game_active:
+        # Draw elements
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, GROUND_POSITION))
 
-    pygame.draw.rect(screen, box_color, score_rectangle)
-    screen.blit(score_surface, score_rectangle)
+        pygame.draw.rect(screen, box_color, score_rectangle)
+        screen.blit(score_surface, score_rectangle)
 
-    # Snail animation
-    snail_rectangle.x -= snail_speed
-    if snail_rectangle.left < -100:
-        snail_rectangle.left = WIDTH
+        # Snail animation
+        snail_rectangle.x -= snail_speed
+        if snail_rectangle.left < -100:
+            snail_rectangle.left = WIDTH
 
-    screen.blit(snail_surface, snail_rectangle)
+        screen.blit(snail_surface, snail_rectangle)
 
-    # Player gravity
-    player_gravity += 1
-    player_rectangle.y += player_gravity
+        # Player gravity
+        player_gravity += 1
+        player_rectangle.y += player_gravity
 
-    if player_rectangle.bottom >= GROUND_POSISTION:
-        player_rectangle.bottom = GROUND_POSISTION
+        if player_rectangle.bottom >= GROUND_POSITION:
+            player_rectangle.bottom = GROUND_POSITION
 
-    screen.blit(player_surface, player_rectangle)
+        screen.blit(player_surface, player_rectangle)
 
-    if player_rectangle.colliderect(snail_rectangle) and has_collided == False:
-        has_collided = True
-        print("Collided")
+        if player_rectangle.colliderect(snail_rectangle) and game_active == True:
+            game_active = False
+            # pygame.quit()
+            # exit()
+    else:
+        screen.fill(box_color)
 
     # Update screen
     pygame.display.update()
